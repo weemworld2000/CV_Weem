22c0c4e28a302b74ceaf9465f56563a51a735134
Weemworld <145841258+weemworld2000@users.noreply.github.com>
2023-10-21 08:40:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ const handleMouseMove = (e) => {
 }
 
 window.addEventListener('mousemove', handleMouseMove);
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
